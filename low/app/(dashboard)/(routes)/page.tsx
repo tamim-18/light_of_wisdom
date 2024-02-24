bc6170e4a2af5389cf9e85ca4ffdb02e9cf85e9f
tamim-18 <tamim11903060@gmail.com>
2024-02-24 21:07:53 +0600

    layout added

--- low/app/(dashboard)/(routes)/page.tsx
+++ low/app/(dashboard)/(routes)/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <UserButton afterSignOutUrl="/"></UserButton>
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
