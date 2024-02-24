ceb53d391c63d400e770b306ce3677dd93f70ec4
tamim-18 <tamim11903060@gmail.com>
2024-02-24 22:04:10 +0600

    css added

---
+++ low/app/(dashboard)/_components/logo.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+import React from "react";
+
+const Logo = () => {
+  return <Image src="/logo.png" alt="logo" width={100} height={100} />;
+};
+
+export default Logo;
--- low/app/(dashboard)/_components/sidebar.tsx
+++ low/app/(dashboard)/_components/sidebar.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import Logo from "./logo";
 
 const Sidebar = () => {
   return (
     <div className=" h-full border-r flex flex-col overflow-y-auto bg-white shadow-sm">
-      Sidebar
+      <div className="p-6">
+        <Logo />
+      </div>
     </div>
   );
 };
