d80d68fccc1ca5f1e5c4249603329ede6e67c4e1
tamim-18 <tamim11903060@gmail.com>
2024-02-26 11:42:57 +0600

    more functionalities added

--- low/components/navbar-routes.tsx
+++ low/components/navbar-routes.tsx
@@ -1,11 +1,31 @@
 "use client";
 import { UserButton } from "@clerk/nextjs";
+import { usePathname, useRouter } from "next/navigation";
 import React from "react";
+import { Button } from "./ui/button";
+import { LogOut } from "lucide-react";
+import Link from "next/link";
 
 const NavbarRoutes = () => {
+  const pathname = usePathname();
+  const router = useRouter();
+  const isTeacherPage = pathname?.startsWith("/teacher");
+  const isPlayerPage = pathname?.includes("/chapter");
   return (
     <div className="flex gap-x-3 ml-auto">
-      <UserButton />
+      {isTeacherPage || isPlayerPage ? (
+        <Button size="sm" variant="ghost">
+          <LogOut className="h-4 w-4 mr-2" />
+          Exit
+        </Button>
+      ) : (
+        <Link href="/teacher/courses">
+          <Button size="sm" variant="ghost">
+            Teacher Mode
+          </Button>
+        </Link>
+      )}
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 };
