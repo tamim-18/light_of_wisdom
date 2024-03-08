92b01b01a81073adbf8cb93467299d5e4c5e7ccd
tamim-18 <tamim11903060@gmail.com>
2024-03-08 11:30:20 +0600

    video adding options are added

---
+++ low/app/(dashboard)/(routes)/teacher/courses/[courseId]/chapter/[chapterId]/_components/chapter-access-form.tsx
@@ -0,0 +1,132 @@
+"use client";
+import React, { useState } from "react";
+import * as z from "zod";
+import axios from "axios";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
+import { db } from "@/lib/db";
+import { cn } from "@/lib/utils";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { Button } from "@/components/ui/button";
+import { Preview } from "@/components/preview";
+import { Input } from "@/components/ui/input";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { Pencil } from "lucide-react";
+import toast from "react-hot-toast";
+import { Textarea } from "@/components/ui/textarea";
+import { Chapter } from "@prisma/client";
+import { Editor } from "@/components/editor";
+import { Checkbox } from "@/components/ui/checkbox";
+
+interface ChapterAccessFormProps {
+  initialData: Chapter;
+  courseId: string;
+  chapterId: string;
+}
+const formSchema = z.object({
+  isFree: z.boolean().default(false),
+});
+
+const ChapterAccessForm = ({
+  initialData,
+  courseId,
+  chapterId,
+}: ChapterAccessFormProps) => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      isFree: !!initialData.isFree,
+    },
+  });
+  const { isSubmitting, isValid } = form.formState;
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      await axios.patch(
+        `/api/courses/${courseId}/chapters/${chapterId}`,
+        values
+      );
+      toast.success("Chapter Updated");
+      toggleEditing();
+      router.refresh();
+    } catch {
+      toast.error("Failed to update course title");
+    }
+  };
+  const [isEditing, setIsEditing] = useState(false);
+  const toggleEditing = () => setIsEditing((prev) => !prev);
+  const router = useRouter();
+
+  return (
+    <div className=" mt-6 bg-slate-100 rounded-md p-4">
+      <div className=" flex font-medium items-center justify-between">
+        Chapter Access Settings
+        <Button onClick={toggleEditing} variant="ghost">
+          {isEditing && <>Cancel</>}
+          {!isEditing && (
+            <>
+              <Pencil className=" h-4 w-4 mr-2" />
+              Edit Access
+            </>
+          )}
+        </Button>
+      </div>
+      {!isEditing && (
+        <p
+          className={cn(
+            "text-sm mt-2",
+            !initialData.isFree && "text-slate-500 italic"
+          )}
+        >
+          {initialData.isFree
+            ? "This chapter is free for preview"
+            : "This chapter is not free for preview"}
+        </p>
+      )}
+
+      {isEditing && (
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className=" space-y-4 mt-4"
+          >
+            <FormField
+              control={form.control}
+              name="isFree"
+              render={({ field }) => (
+                <FormItem className="flex flex-row items-start space-x-3 space-y-0 rounded-md border p-4">
+                  <FormControl>
+                    <Checkbox
+                      checked={field.value}
+                      onCheckedChange={field.onChange}
+                    />
+                  </FormControl>
+                  <div className="space-y-1 leading-none">
+                    <FormDescription>
+                      Check this box if you want to make this chapter free for
+                      preview
+                    </FormDescription>
+                  </div>
+                </FormItem>
+              )}
+            />
+            <div className=" flex items-center gap-x-2 ">
+              <Button type="submit" disabled={!isValid || isSubmitting}>
+                Save
+              </Button>
+            </div>
+          </form>
+        </Form>
+      )}
+    </div>
+  );
+};
+
+export default ChapterAccessForm;
--- low/app/(dashboard)/(routes)/teacher/courses/[courseId]/chapter/[chapterId]/page.tsx
+++ low/app/(dashboard)/(routes)/teacher/courses/[courseId]/chapter/[chapterId]/page.tsx
@@ -1,12 +1,13 @@
 import { IconBadge } from "@/components/icon-badge";
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
-import { ArrowLeftCircle, LayoutDashboardIcon } from "lucide-react";
+import { ArrowLeftCircle, Eye, LayoutDashboardIcon, Video } from "lucide-react";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 import React from "react";
 import ChapterTitleForm from "./_components/chapter-title-form";
 import ChapterDescriptionForm from "./_components/chapter-description-from";
+import ChapterAccessForm from "./_components/chapter-access-form";
 
 const ChapterId = async ({
   params,
@@ -67,6 +68,23 @@ const ChapterId = async ({
               courseId={params.courseId}
               chapterId={params.chapterId}
             />
+          </div>
+          <div className=" ">
+            <div className=" flex items-center gap-x-2">
+              <IconBadge icon={Eye} />
+              <h2 className=" text-xl"> Access Settings</h2>
+            </div>
+            <ChapterAccessForm
+              initialData={chapter}
+              courseId={params.courseId}
+              chapterId={params.chapterId}
+            />
+          </div>
+        </div>
+        <div>
+          <div className=" flex items-center gap-x-2">
+            <IconBadge icon={Video} />
+            <h2 className=" text-xl"> Add chapter Video</h2>
           </div>
         </div>
       </div>
