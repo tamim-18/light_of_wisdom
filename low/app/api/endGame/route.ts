5221be394d7e05f26d90a7a252850164db24c849
tamim-18 <tamim11903060@gmail.com>
2024-03-10 14:27:55 +0600

    exam api adedd

---
+++ low/app/api/endGame/route.ts
@@ -0,0 +1,44 @@
+import { db } from "@/lib/db";
+import { endGameSchema } from "@/schemas/question";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request, res: Response) {
+  try {
+    const body = await req.json();
+    const { gameId } = endGameSchema.parse(body);
+
+    const game = await db.game.findUnique({
+      where: {
+        id: gameId,
+      },
+    });
+    if (!game) {
+      return NextResponse.json(
+        {
+          message: "Game not found",
+        },
+        {
+          status: 404,
+        }
+      );
+    }
+    await db.game.update({
+      where: {
+        id: gameId,
+      },
+      data: {
+        timeEnded: new Date(),
+      },
+    });
+    return NextResponse.json({
+      message: "Game ended",
+    });
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: "Something went wrong",
+      },
+      { status: 500 }
+    );
+  }
+}
